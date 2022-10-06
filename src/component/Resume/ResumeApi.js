c0f21208a5a1caff76c3b22a776f927fe53a3e84
Adarsh Trivedi <adarsh.trivedi_cs19@gla.ac.in>
2022-10-06 13:54:04 +0530

    portfoio website

---
+++ src/component/Resume/ResumeApi.js
@@ -0,0 +1,52 @@
+const ResumeApi = [
+    {
+      id: 1,
+      category: "education",
+      year: "SKD Academy (CBSE) (2017 - 2018)",
+      title: "High School",
+      desc: "The education should be very interactual. Ut tincidunt est ac dolor aliquam sodales. Phasellus sed mauris hendrerit, laoreet sem in, lobortis mauris hendrerit ante.",
+      rate: "4.30/5",
+    },
+    {
+      id: 2,
+      category: "education",
+      year: "SKD Academy (CBSE) (2018 - 2019) ",
+      title: "Higher Secondary Certificate",
+      desc: "Maecenas finibus nec sem ut imperdiet. Ut tincidunt est ac dolor aliquam sodales. Phasellus sed mauris hendrerit, laoreet sem in, lobortis mauris hendrerit ante.",
+      rate: "4.50/5",
+    },
+    {
+      id: 3,
+      category: "GLA University",
+      year: "University of Studies (2019 - 2023) ",
+      title: "Tips For Personal Portfolio",
+      desc: "If you are going to use a passage. Ut tincidunt est ac dolor aliquam sodales. Phasellus sed mauris hendrerit, laoreet sem in, lobortis mauris hendrerit ante.",
+      rate: "4.80/5 ",
+    },
+    {
+      id: 4,
+      category: "experience",
+      year: "BSE In CSE (2004 - 2008) ",
+      title: "Diploma in Web Development",
+      desc: "Contrary to popular belief. Ut tincidunt est ac dolor aliquam sodales. Phasellus sed mauris hendrerit, laoreet sem in, lobortis mauris hendrerit ante.",
+      rate: "4.70/5 ",
+    },
+    {
+      id: 5,
+      category: "experience",
+      year: "Job at Rainbow-Themes (2008 - 2016) ",
+      title: "The Personal Portfolio Mystery",
+      desc: "Generate Lorem Ipsum which looks. Ut tincidunt est ac dolor aliquam sodales. Phasellus sed mauris hendrerit, laoreet sem in, lobortis mauris hendrerit ante.",
+      rate: "4.95/5 ",
+    },
+    {
+      id: 6,
+      category: "experience",
+      year: "Works at Plugin Development (2016 - 2020) ",
+      title: "Diploma in Computer Science",
+      desc: "Maecenas finibus nec sem ut imperdiet. Ut tincidunt est ac dolor aliquam sodales. Phasellus sed mauris hendrerit, laoreet sem in, lobortis mauris hendrerit ante.",
+      rate: "5.00/5 ",
+    },
+  ]
+  
+  export default ResumeApi
